05d1a8079d0aadc48addf88c00c821f0cb379fa3
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-02-19 11:45:39 +0100

    add icon

--- src/app/components/ui/header.status.tsx
+++ src/app/components/ui/header.status.tsx
@@ -1,10 +1,10 @@
-import { ChatBubbleLeftIcon } from "@heroicons/react/24/solid"
+import { ChatBubbleLeftIcon, UserIcon } from "@heroicons/react/24/solid"
 import Badge from "./partials/badge"
 
 const headerStatus = () => {
     return (<div className="flex gap-2">
         <Badge>
-            <ChatBubbleLeftIcon className="rounded-full bg-white p-1" width={32} />
+            <UserIcon className="rounded-full bg-white p-1" width={32} />
         </Badge>
         <Badge>
             <ChatBubbleLeftIcon className="rounded-full bg-white p-1" width={32} />
