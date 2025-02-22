51bdb4bfbb69b00e7a19da3cd1fcca07cf4ea343
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-02-22 23:37:35 +0100

    add disabled prop

--- src/app/components/ui/partials/button.tsx
+++ src/app/components/ui/partials/button.tsx
@@ -1,10 +1,11 @@
 interface ButtonProps {
     value: string
+    disabled?: boolean
 };
 
-const Button = ({ value } : ButtonProps) => {
+const Button = ({ value, disabled = false } : ButtonProps) => {
     return (
-        <button className="btn rounded-full bg-slate-900 text-white pr-8 pl-8 shadow-md h-11 hover:shadow-lg hover:bg-black active:border-solid active:border">
+        <button className="btn rounded-full bg-slate-900 text-white pr-8 pl-8 shadow-md h-11 hover:shadow-lg hover:bg-black active:border-solid active:border disabled:opacity-10 disabled:active:border-none" disabled={disabled}>
             {value}
         </button>
     );
