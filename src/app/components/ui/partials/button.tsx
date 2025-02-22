4a70a6bcd5e0c1ac1a81ca5de7c77ba9ea8138b8
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-02-22 19:51:47 +0100

    fancy button

--- src/app/components/ui/partials/button.tsx
+++ src/app/components/ui/partials/button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 const Button = ({ value } : ButtonProps) => {
     return (
-        <button className="btn rounded-full bg-black text-white pr-8 pl-8 shadow-md h-11">
+        <button className="btn rounded-full bg-slate-900 text-white pr-8 pl-8 shadow-md h-11 hover:shadow-lg hover:bg-black active:border-solid active:border">
             {value}
         </button>
     );
