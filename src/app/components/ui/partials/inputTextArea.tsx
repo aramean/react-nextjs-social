7e8fffdf6faa92b887dcd63bcbca7fa3c9cd4892
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-01 00:20:48 +0100

    cleanup

--- src/app/components/ui/partials/inputTextArea.tsx
+++ src/app/components/ui/partials/inputTextArea.tsx
@@ -17,7 +17,7 @@ const InputTextArea = ({ value = "", placeholder = "", height = "md", textSize =
 
   return (
     <textarea
-      className={`flex p-3 line-clamp-3 border border-spacing-1 rounded-md border-slate-500 shadow-sm w-${width} focus:shadow-md focus:bg-white focus:border-slate-800 ${heightClass} text-${textSize}`}
+      className={`flex p-3 border border-spacing-1 rounded-md border-slate-500 shadow-sm w-${width} focus:shadow-md focus:bg-white focus:border-slate-800 ${heightClass} text-${textSize}`}
       value={value}
       placeholder={placeholder}
       onChange={onChange}>
