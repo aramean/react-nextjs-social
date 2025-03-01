a2b164a7b38a656e0c19ae4e995a30d50eb41510
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-01 23:59:55 +0100

    add constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,4 @@
 export const SESSION_COOKIE = "a_session"
+export const KEY = process.env.NEXT_APPWRITE_KEY as string
+export const ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string
+export const PROJECT = process.env.NEXT_PUBLIC_APPWRITE_PROJECT as string
