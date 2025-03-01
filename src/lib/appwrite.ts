521ddc189b9d80c58dad3a50cc73c4c6c8114894
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-01 12:40:57 +0100

    cleanup

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,9 +1,6 @@
 import { Client, Account, Databases, ID, Query } from "appwrite"
 
 const client = new Client()
-client.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string)
-
-client
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string)
 
