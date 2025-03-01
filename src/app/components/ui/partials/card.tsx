ad937e70f9c2aee8d34ae7818600111e087e0dc4
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-01 11:15:32 +0100

    adjust design

--- src/app/components/ui/partials/card.tsx
+++ src/app/components/ui/partials/card.tsx
@@ -17,14 +17,14 @@ const Card = ({ picture = "", title = "", text = "", createdAt }: CardProps) => 
       />
     )}
     <div className="border-r border-b border-l border-gray-300 lg:border-t lg:border-gray-200 rounded-b p-5 flex flex-col justify-between leading-normal shadow-sm mt-3">
-      <div className="flex items-center">
+      <div className="flex">
         <UserIcon className="w-11 h-10 mr-2 mb-1 p-1 rounded-full bg-slate-200 text-white" />
-        <div className="text-sm w-full flex flex-col">
+        <div className="text-sm w-full mt-2 flex flex-col">
           <p className="text-gray-900 leading-none">Josef Gabrielsson</p>
           <p className="text-gray-600 text-xs">{createdAt} * <LockClosedIcon className='mr-1 w-3 inline-flex' /></p>
         </div>
-        <p className="text-sm text-gray-600 flex items-center">
-          <EllipsisHorizontalIcon width="12" className='mr-1' />
+        <p className="text-sm text-gray-600 flex place-items-start">
+          <EllipsisHorizontalIcon className='w-5' />
         </p>
       </div>
       <div className="mb-1">
