e1e79d90f880facc0090be2220094017c7d9904d
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 17:54:01 +0100

    format

--- src/components/partials/button.tsx
+++ src/components/partials/button.tsx
@@ -1,9 +1,9 @@
 interface ButtonProps {
-    value: string
-    disabled?: boolean
+  value: string
+  disabled?: boolean
 };
 
-const Button = ({ value, disabled = false } : ButtonProps) => {
+const Button = ({ value, disabled = false }: ButtonProps) => {
   return (
     <button className="btn rounded-full bg-slate-900 text-white pr-8 pl-8 shadow-md h-11 hover:shadow-lg hover:bg-black active:border-solid active:border disabled:opacity-10 disabled:active:border-none" disabled={disabled}>
       {value}
