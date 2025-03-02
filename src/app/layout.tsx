55f60700d35ac6c1a672f862803881e3e3f578a7
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 09:12:03 +0100

    format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,15 @@ import type { Metadata } from "next"
 import { APP_TITLE } from "@constants"
 import "@css"
 
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
 export const metadata: Metadata = {
   title: APP_TITLE
 }
 
-export default function RootLayout({
-  children
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className="bg-slate-50">
