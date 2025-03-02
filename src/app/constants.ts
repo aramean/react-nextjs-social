79a91d630d7e6bda3eeb9826b9efb951937bfc64
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 16:24:17 +0100

    chanhe title

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const APP_TITLE = "nextSocial"
+export const APP_TITLE = "Nextsocial"
 export const SESSION_COOKIE = "a_session"
 export const KEY = process.env.NEXT_APPWRITE_KEY as string
 export const ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string
