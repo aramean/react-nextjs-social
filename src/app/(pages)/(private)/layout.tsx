d7886465599b78973c4cb5bdff7a21cd872f4d7d
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 21:04:31 +0100

    fix link

--- src/app/(pages)/(private)/layout.tsx
+++ src/app/(pages)/(private)/layout.tsx
@@ -13,7 +13,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
 
   return (<>
     <header className="flex bg-black border-b border-gray-200 shadow-md gap-4 fixed top-0 inset-x-0 z-100 h-16 items-center px-4">
-      <Link href="dashboard">
+      <Link href="/dashboard">
         <Logo color="#ffffff" size="30" />
       </Link>
       <HeaderSearch />
