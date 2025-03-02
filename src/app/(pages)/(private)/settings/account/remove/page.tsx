78898a858421d89925131da01c1cf204022c26fa
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 21:45:33 +0100

    add new section

---
+++ src/app/(pages)/(private)/settings/account/remove/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import SettingsForm from "@/components/forms/settings"
+import Alert from "@/components/partials/alert"
+import Box from "@/components/partials/box"
+import Button from "@/components/partials/button"
+import Sidebar from "@/components/partials/sidebar"
+import { useAccount } from "@/hooks/useAccount"
+
+export default function SettingsName() {
+  const { isLoading, error, updateName } = useAccount()
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    return await updateName("test")
+  }
+
+  return (<div className="flex">
+    <div className="w-2/5">
+      <Sidebar />
+    </div>
+    <div className="w-3/5">
+      <Button value="Back" />Account Management
+
+      <Box>
+        <SettingsForm />
+      </Box>
+
+      {error && <Alert message={error} />}
+      <button onClick={handleSubmit} disabled={isLoading}>Save</button>
+    </div>
+  </div>)
+}
