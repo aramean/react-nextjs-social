68b3b5ccc61cd7d53d5ccdaba7eeb83253af68fa
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 21:40:41 +0100

    fix component

--- src/components/partials/br.tsx
+++ src/components/partials/br.tsx
@@ -1,17 +1,19 @@
 interface BrProps {
-    height?: "none" | "sm" | "md" | "lg" | "xl"
+  height?: "none" | "xs" | "sm" | "md" | "lg" | "xl" | "xxl"
 }
 
 const Br = ({ height = "none" }: BrProps) => {
   const heightClass = {
     none: "h-0",
-    sm: "h-0",
-    md: "h-10",
-    lg: "h-11",
-    xl: "h-12"
+    xs: "h-0.5",
+    sm: "h-1",
+    md: "h-2",
+    lg: "h-3",
+    xl: "h-4",
+    xxl: "h-5"
   }[height]
 
-  return (<br className={`content-[''] ${heightClass}`}></br>)
+  return (<br className={`flex content-[''] ${heightClass}`}></br>)
 }
 
 export default Br
