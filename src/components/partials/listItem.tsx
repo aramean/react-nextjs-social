1482219efe4b34f0e11e4c7b70eb50f06bf0af10
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 22:03:50 +0100

    cleanup

--- src/components/partials/listItem.tsx
+++ src/components/partials/listItem.tsx
@@ -3,21 +3,13 @@ import Link from "next/link"
 
 interface ListeItemProps {
   href?: string
-  picture?: string
   title?: string
   text?: string
   value?: string
 }
 
-const ListItem = ({ href = "", picture = "", title = "", text = "", value }: ListeItemProps) => {
+const ListItem = ({ href = "", title = "", text = "", value }: ListeItemProps) => {
   return (<div className="width-full bg-white rounded-md">
-    {picture && (
-      <div
-        className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-        title="."
-        style={{ backgroundImage: `url(${picture})` }}
-      />
-    )}
     <Link href={href} className="font-normal hover:no-underline">
       <div className="flex flex-col justify-between border-r border-b border-l border-gray-300 lg:border-t lg:border-gray-200 rounded-b p-3 shadow-sm hover:bg-slate-50">
         <div className="flex flex-row items-center">
