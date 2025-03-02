5369dbd16c6c9db06690f3477ed934976e5cf56e
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 01:23:43 +0100

    fix layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -64,7 +64,7 @@ export default function Login() {
 
   return (
     <div className="grid grid-rows-[auto_auto_auto] items-center justify-items-center min-h-screen">
-      <main className="flex flex-col gap-5 row-start-2 bg-slate-50 sm:w-96 md:w-full max-w-md items-center p-8 md:p-10 lg:p-12 rounded-xl shadow-lg">
+      <main className="flex flex-col gap-5 row-start-2 bg-slate-50 sm:w-full md:w-full max-w-md p-8 sm:p-12 md:p-12 lg:p-12 rounded-xl shadow-lg">
         <FormLogin
           email={email}
           setEmail={setEmail}
