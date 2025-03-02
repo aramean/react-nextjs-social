166b772c8c98c0990bb0626116f79aab61325540
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 21:45:22 +0100

    rename

--- src/app/(pages)/(private)/settings/account/name/page.tsx
+++ src/app/(pages)/(private)/settings/account/name/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import SettingsForm from "@/components/forms/settings"
 import Alert from "@/components/partials/alert"
 import Box from "@/components/partials/box"
 import Button from "@/components/partials/button"
@@ -21,7 +22,9 @@ export default function SettingsName() {
     <div className="w-3/5">
       <Button value="Back" />Profile information
 
-      <Box title="Name"></Box>
+      <Box>
+        <SettingsForm />
+      </Box>
 
       {error && <Alert message={error} />}
       <button onClick={handleSubmit} disabled={isLoading}>Save</button>
