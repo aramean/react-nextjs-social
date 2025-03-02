e55d8d7b9d82f43c7bbfd27b242fd0d7e913160e
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-03 00:05:36 +0100

    cleanup

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 export const APP_TITLE = "Nextsocial"
-export const SESSION_COOKIE = "a_session"
+
 export const KEY = process.env.NEXT_APPWRITE_KEY as string
 export const ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string
 export const PROJECT = process.env.NEXT_PUBLIC_APPWRITE_PROJECT as string
