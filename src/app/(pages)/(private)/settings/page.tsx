d8fd158615e3b75056d95596853ba2074d6fbb63
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-02 18:26:42 +0100

    open in new page

--- src/app/(pages)/(private)/settings/page.tsx
+++ src/app/(pages)/(private)/settings/page.tsx
@@ -1,15 +1,9 @@
 "use client"
 
+import ListItem from "@/components/partials/listItem"
 import Sidebar from "@/components/partials/sidebar"
-import { useAccount } from "@/hooks/useAccount"
 
 export default function Settings() {
-  const { isLoading, error, updateName } = useAccount()
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault()
-    return await updateName("test")
-  }
 
   return (<div className="flex">
     <div className="w-2/5">
@@ -17,8 +11,7 @@ export default function Settings() {
     </div>
     <div className="w-3/5">
       Profile information
-      
-      <button onClick={handleSubmit}>Save</button>
+      <ListItem href="settings/name" title="Name" value="Josef"></ListItem>
     </div>
   </div>)
 }
