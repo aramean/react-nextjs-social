42ffa1ebd55e8126532b38db7a0d7e0e9e485a42
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-06 22:19:02 +0100

    add title and direction

--- src/components/partials/box.tsx
+++ src/components/partials/box.tsx
@@ -1,15 +1,18 @@
 interface BoxProps {
+  title?: string
+  direction?: "col" | "row"
   children: React.ReactNode
 }
 
-const Box = ({ children }: BoxProps) => {
-  return (<div className="width-full bg-white rounded-md">
-    <div className="border-r border-b border-l border-gray-200 rounded-b p-5 flex flex-col justify-between shadow-sm mt-3">
-      <div className="flex flex-row">
+const Box = ({ title, direction = "col", children }: BoxProps) => {
+  return (
+    <div className="flex flex-col bg-white border-r border-b border-l border-gray-200 rounded-b p-5 justify-between shadow-sm mt-3">
+      <div className={`flex flex-${direction}`}>
+        {title}
         {children}
       </div>
     </div>
-  </div>)
+  )
 }
 
 export default Box
