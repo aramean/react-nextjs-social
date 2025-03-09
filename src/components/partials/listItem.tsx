631d9ceea5006d217298ed062961c4d701f527d7
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-09 19:53:44 +0100

    fix layout

--- src/components/partials/listItem.tsx
+++ src/components/partials/listItem.tsx
@@ -13,7 +13,7 @@ const ListItem = ({ href = "", title = "", text = "", value }: ListeItemProps) =
     <Link href={href} className="font-normal hover:no-underline">
       <div className="flex flex-col justify-between border-r border-b border-l border-gray-200 rounded-b p-3 shadow-sm hover:bg-slate-50">
         <div className="flex flex-row items-center">
-          {title && (<div className="text-gray-900 font-bold ml-2">{title}</div>)}
+          {title && (<div className="text-gray-900 font-bold w-64 ml-2">{title}</div>)}
           <p className="text-gray-900 mr-24">{text}</p>
           <p className="text-gray-600 w-full">{value}</p>
           <p className="text-sm pr-1 text-gray-600">
