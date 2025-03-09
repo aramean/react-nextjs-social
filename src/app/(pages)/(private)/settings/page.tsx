6551ef8c976c06d5dc5217502d4bdfe5d2da81a4
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-09 19:53:23 +0100

    add descriptions

--- src/app/(pages)/(private)/settings/page.tsx
+++ src/app/(pages)/(private)/settings/page.tsx
@@ -9,11 +9,12 @@ export default function Settings() {
   return (<>
     <Heading text="Profile information" size="2xl"></Heading>
     <Br height="xl" />
-    <ListItem href="settings/account/name" title="Name" value="Josef"></ListItem>
-    <ListItem href="settings/account/email" title="Email" value="@"></ListItem>
+    <ListItem href="settings/account/picture" title="Picture" value="Upload or change your profile picture."></ListItem>
+    <ListItem href="settings/account/name" title="Name" value="Edit your full name."></ListItem>
+    <ListItem href="settings/account/email" title="Email" value="Update your email address."></ListItem>
     <Br height="xxl" />
     <Heading text="Account management" size="2xl"></Heading>
     <Br height="xl" />
-    <ListItem href="settings/account/remove" title="" value="Remove account"></ListItem>
+    <ListItem href="settings/account/remove" title="Remove account" value="Permanently delete your account and data"></ListItem>
   </>)
 }
