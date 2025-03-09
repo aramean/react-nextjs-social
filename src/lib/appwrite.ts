d9af9491fb6d2c51c900a95cdfd21b724a803edd
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-09 19:51:59 +0100

    support storage

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,9 +1,10 @@
 "use client"
 
-import { Client, Account, Databases, ID, Query, AppwriteException } from "appwrite"
+import { Client, Storage, Account, Databases, ID, Query, AppwriteException } from "appwrite"
 import { ENDPOINT, PROJECT } from "@constants"
 
 const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT)
+const storage = new Storage(client)
 const account = new Account(client)
 const databases = new Databases(client)
 
@@ -11,4 +12,4 @@ const exception = (error: unknown): string => {
   return error instanceof AppwriteException ? error.message : "An unexpected error occurred"
 }
 
-export { client, account, databases, ID, Query, exception }
+export { client, storage, account, databases, ID, Query, exception }
