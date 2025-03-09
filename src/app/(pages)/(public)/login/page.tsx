e7163c4bb0682b6fd8f2c88da04687ab6fce0754
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-09 20:28:31 +0100

    support xs

--- src/app/(pages)/(public)/login/page.tsx
+++ src/app/(pages)/(public)/login/page.tsx
@@ -5,7 +5,7 @@ import FormLogin from "@/components/forms/signin"
 export default function Login() {
   return (
     <div className="grid grid-rows-[auto_auto_auto] items-center justify-items-center min-h-screen">
-      <main className="flex flex-col gap-5 row-start-2 bg-slate-50 sm:w-full md:w-full max-w-md p-8 sm:p-12 md:p-12 lg:p-12 rounded-xl shadow-lg">
+      <main className="flex flex-col gap-5 row-start-2 bg-slate-50 sm:w-full md:w-full max-w-md p-8 sm:p-12 md:p-12 lg:p-12 rounded-xl shadow-none xs:shadow-lg">
         <FormLogin />
       </main>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)"
+      },
+      screens: {
+        "xs": "30rem"
       }
     }
   },
