6ff822f3502bb561a003d21a6660829c52560851
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-14 23:04:13 +0100

    cleanup

--- src/components/partials/button.tsx
+++ src/components/partials/button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 const Button = ({ value, disabled = false }: ButtonProps) => {
   return (
     <button
-      className="btn rounded-full bg-slate-900 text-white pr-8 pl-8 shadow-md h-11 hover:shadow-lg hover:bg-black active:border-solid active:border disabled:opacity-10 disabled:active:border-none"
+      className="btn rounded-full bg-slate-900 text-white font-semibold pr-8 pl-8 shadow-md h-11 hover:shadow-lg hover:bg-black active:border-solid active:border disabled:opacity-10 disabled:active:border-none"
       disabled={disabled}
     >
       {value}
