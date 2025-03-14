29b6363bf857dca1bb87fe9ef567012105ea7b66
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-15 00:11:55 +0100

    add current user id

--- src/components/header.status.tsx
+++ src/components/header.status.tsx
@@ -1,20 +1,20 @@
 "use client"
 
 import { useRouter } from "next/navigation"
+import { getAuthToken } from "@/utils/auth"
 import { useLogout } from "@/hooks/useLogout"
 import { ChatBubbleLeftIcon, UserIcon, BellIcon } from "@heroicons/react/24/solid"
 import Badge from "@/components/partials/badge"
 import Link from "next/link"
 import DropDown from "@/components/partials/dropdown"
-//import { deleteAuthToken } from "@/utils/cookies"
 
 const HeaderStatus = () => {
+
   const { logout } = useLogout()
   const router = useRouter()
 
   const handleLogout = async () => {
     await logout()
-    //deleteAuthToken()
   }
 
   return (
@@ -42,7 +42,7 @@ const HeaderStatus = () => {
         ]}
         onSelect={(option) => {
           if (option.value === "profile") {
-            router.push("profile")
+            router.push("/profile/" + getAuthToken())
           } else if (option.value === "settings") {
             router.push("/settings")
           } else if (option.value === "logout") {
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,11 @@
 "use client"
 
-export const getAuthToken = (): string | null => {
+export const getAuthToken = (): string => {
   const item = localStorage.getItem("cookieFallback") || ""
   try {
     const firstKey = Object.keys(JSON.parse(item))[0]
     return firstKey.split("_")[2]
   } catch {
-    return null
+    return ""
   }
 }
