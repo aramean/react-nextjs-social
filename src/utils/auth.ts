6dd4e76c744ca0e9077cb74a6df8252807819025
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-15 14:18:30 +0100

    run only on client

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,7 @@
 "use client"
 
-export const getAuthToken = (): string => {
-  const item = localStorage.getItem("cookieFallback") || ""
+export const getAuthToken = () => {
+  const item = (typeof window !== "undefined") ? localStorage.getItem("cookieFallback") || "" : ""
   try {
     const firstKey = Object.keys(JSON.parse(item))[0]
     return firstKey.split("_")[2]
