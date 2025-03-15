860479e82282967a6a08768616b9ff7784673a08
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-15 19:57:12 +0100

    use jwt instead

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,9 +2,11 @@
 
 export const getAuthToken = () => {
   const item = (typeof window !== "undefined") ? localStorage.getItem("cookieFallback") || "" : ""
+  const jwt = item && Object.values(JSON.parse(item))[0] as string
+
   try {
-    const firstKey = Object.keys(JSON.parse(item))[0]
-    return firstKey.split("_")[2]
+    const decode = JSON.parse(atob(jwt))
+    return decode.id
   } catch {
     return ""
   }
