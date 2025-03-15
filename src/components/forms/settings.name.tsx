7ec05aed8b7751fa8045ccf3040033750d1d712b
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-15 11:23:55 +0100

    add fields

--- src/components/forms/settings.name.tsx
+++ src/components/forms/settings.name.tsx
@@ -8,19 +8,31 @@ import Alert from "@/components/partials/alert"
 import Button from "@/components/partials/button"
 
 const SettingsFormName = () => {
-  const [name, setName] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [middleName, setMiddleName] = useState("")
+  const [lastName, setLastName] = useState("")
   const { isLoading, error, updateName } = useAccount()
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
-    return await updateName(name)
+    return await updateName(firstName)
   }
 
   return (<Form onSubmit={handleSubmit} className="flex flex-col w-full gap-4">
     <InputText
-      placeholder="Name"
-      value={name}
-      onChange={(e) => setName(e.target.value)}
+      placeholder="First Name"
+      value={firstName}
+      onChange={(e) => setFirstName(e.target.value)}
+    />
+    <InputText
+      placeholder="Middle Name"
+      value={middleName}
+      onChange={(e) => setMiddleName(e.target.value)}
+    />
+    <InputText
+      placeholder="Last Name"
+      value={lastName}
+      onChange={(e) => setLastName(e.target.value)}
     />
     {error && <Alert message={error} />}
     <Button disabled={isLoading} value="Save" />
