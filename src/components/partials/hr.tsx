3edd647581e4d6cd628cc67716f3df015aebd4be
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-03-21 20:57:06 +0100

    add size

--- src/components/partials/hr.tsx
+++ src/components/partials/hr.tsx
@@ -1,7 +1,17 @@
 "use client"
 
-const Hr = () => {
-  return (<hr className="mt-2 mb-2"></hr>)
+interface HrProps {
+  size?: "xs" | "md" | "lg"
+}
+
+const Hr = ({ size = "md" }: HrProps) => {
+  const sizeClass = {
+    xs: "my-1",
+    md: "my-2",
+    lg: "my-3"
+  }[size]
+
+  return (<hr className={sizeClass}></hr>)
 }
 
 export default Hr
